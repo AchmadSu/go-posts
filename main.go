90a1ecf5c81742abadefb0d974cb3bc668a7bbda
AchmadSu <ecepentis@gmail.com>
2025-11-04 00:00:36 +0700

    add cors configuration

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"go-posts/database"
 	"go-posts/routes"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -14,6 +15,7 @@ func main() {
 	database.ConnectDB()
 
 	r := gin.Default()
+	r.Use(cors.Default())
 	routes.SetupRoutes(r)
 
 	fmt.Println("Server start on :8080")
